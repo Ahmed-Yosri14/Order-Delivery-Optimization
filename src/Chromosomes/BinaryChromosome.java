2c59134bbc507077693ed23ac31516139e469b78
Mohamed <mohamedmahmoud1549@gmail.com>
2025-10-18 19:46:37 +0300

    Implemented encoding and fitness evaluation

--- src/Chromosomes/BinaryChromosome.java
+++ src/Chromosomes/BinaryChromosome.java
@@ -1,18 +1,125 @@
 package Chromosomes;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class BinaryChromosome implements Chromosome {
-    List<Boolean>genes;
-    @Override
-    public void mutate(double probability) {
+    private List<List<Boolean>> genes; // 2D array: rows=orders, cols=positions
+    private Integer fitness;
+    private static final Random random = new Random();
 
+    public BinaryChromosome() {
+        this.genes = new ArrayList<>();
+        this.fitness = (Integer) 0;
     }
 
     @Override
     public void generateGenes(int numberOfGenes) {
+        genes.clear();
+
+        // Initialize 2D array with all false
         for (int i = 0; i < numberOfGenes; i++) {
-            genes.add(Math.random()>0.5);
+            List<Boolean> row = new ArrayList<>();
+            for (int j = 0; j < numberOfGenes; j++) {
+                row.add(Boolean.valueOf(false));
+            }
+            genes.add(row);
         }
+
+        // Create a random permutation of ALL orders
+        List<Integer> orderIndices = new ArrayList<>();
+        for (int i = 0; i < numberOfGenes; i++) {
+            orderIndices.add(Integer.valueOf(i));
+        }
+
+        // Shuffle to create random permutation
+        Collections.shuffle(orderIndices, random);
+
+        // Assign each order to its position in the permutation
+        for (int position = 0; position < numberOfGenes; position++) {
+            int orderIndex = orderIndices.get(position);
+            genes.get(orderIndex).set(position, Boolean.valueOf(true));
+        }
+    }
+
+    @Override
+    public void mutate(double probability) {
+        // Mutation implementation will be added later
+    }
+
+    // Getters
+    public List<List<Boolean>> getGenes() {
+        return genes;
+    }
+
+    public void setGenes(List<List<Boolean>> genes) {
+        this.genes = new ArrayList<>();
+        for (List<Boolean> row : genes) {
+            this.genes.add(new ArrayList<>(row));
+        }
+    }
+
+    public Integer getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(Integer fitness) {
+        this.fitness = fitness;
+    }
+
+    // Get delivery sequence as list of order indices
+    public List<Integer> getDeliverySequence() {
+        List<Integer> sequence = new ArrayList<>();
+        int numOrders = genes.size();
+
+        // For each position (column)
+        for (int position = 0; position < numOrders; position++) {
+            // Find which order (row) has 1 at this position
+            for (int order = 0; order < numOrders; order++) {
+                if (genes.get(order).get(position)) {
+                    sequence.add(Integer.valueOf(order));
+                    break;
+                }
+            }
+        }
+
+        return sequence;
+    }
+
+    // Get number of orders to be delivered
+    public int getNumberOfDeliveries() {
+        return getDeliverySequence().size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("BinaryChromosome{\n");
+
+        for (int i = 0; i < genes.size(); i++) {
+            sb.append("  Order ").append(i).append(": [");
+            for (int j = 0; j < genes.get(i).size(); j++) {
+                sb.append(genes.get(i).get(j) ? "1" : "0");
+                if (j < genes.get(i).size() - 1) sb.append(", ");
+            }
+            sb.append("]\n");
+        }
+
+        sb.append("  Delivery sequence: ").append(getDeliverySequence()).append("\n");
+        sb.append("  Number of deliveries: ").append(getNumberOfDeliveries()).append("\n");
+        sb.append("  Fitness: ").append(fitness).append("\n");
+        sb.append("}");
+
+        return sb.toString();
+    }
+
+    // Create a deep copy of this chromosome
+    public BinaryChromosome clone() {
+        BinaryChromosome copy = new BinaryChromosome();
+        copy.setGenes(this.genes);
+        copy.setFitness(this.fitness);
+        return copy;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import Chromosomes.BinaryChromosome;
 import Chromosomes.Chromosome;
+import Fitness.*;
 
 import java.util.*;
 
@@ -6,35 +8,108 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Choose the Chromosome type");
-        System.out.println("1-Binary \n 2-Integer\n 3-Floating Point");
-        int choice,noOfOrders,popSize;
+        System.out.println("1-Binary \n2-Integer\n3-Floating Point");
+        int choice, noOfOrders, popSize, timeConstraint;
+
         choice = sc.nextInt();
+
         System.out.println("Please enter the number of orders");
         noOfOrders = sc.nextInt();
+
         System.out.println("Please enter the population size");
         popSize = sc.nextInt();
-        Initializer initializer = new Initializer();
-        List<Chromosome> population = initializer.init(choice,noOfOrders,popSize);
-        ArrayList<ArrayList<Integer>> distMatrix = getDistanceBetweenAllPoints(noOfOrders);
-    }
 
+        System.out.println("Please enter the time constraint");
+        timeConstraint = sc.nextInt();
+
+        // Generate distance matrix (n+1 for depot + orders)
+        ArrayList<ArrayList<Integer>> distMatrix = getDistanceBetweenAllPoints(noOfOrders + 1);
+
+        System.out.println("\n=== Distance Matrix ===");
+        printDistanceMatrix(distMatrix);
+
+        // Initialize population
+        Initializer initializer = new Initializer();
+        List<Chromosome> population = initializer.init(choice, noOfOrders, popSize);
+
+        System.out.println("\n=== Initial Population ===");
+
+        // For binary chromosomes, evaluate fitness
+        if (choice == 1) {
+            FitnessEvaluator evaluator = new BinaryFitnessEvaluator(distMatrix, timeConstraint);
+
+            for (int i = 0; i < population.size(); i++) {
+                BinaryChromosome chromosome = (BinaryChromosome) population.get(i);
+
+                // Calculate fitness
+                Integer fitness = evaluator.evaluate(chromosome);
+                chromosome.setFitness(fitness);
+
+                // Calculate total route time
+                int totalTime = ((BinaryFitnessEvaluator) evaluator).calculateTotalRouteTime(chromosome);
+
+                System.out.println("\nChromosome " + (i + 1) + ":");
+                System.out.println(chromosome);
+                System.out.println("  Total route time: " + totalTime + " (constraint: " + timeConstraint + ")");
+                System.out.println("  Orders deliverable within time: " + (int)fitness + "/" + noOfOrders);
+            }
+
+            // Find best chromosome
+            BinaryChromosome best = (BinaryChromosome) population.get(0);
+            for (Chromosome c : population) {
+                BinaryChromosome bc = (BinaryChromosome) c;
+                if (bc.getFitness() > best.getFitness()) {
+                    best = bc;
+                }
+            }
+
+            System.out.println("\n=== Best Initial Solution ===");
+            System.out.println("Fitness: " + best.getFitness() + " orders");
+            System.out.println("Delivery sequence: " + best.getDeliverySequence());
+        } else {
+            // Display other chromosome types
+            for (int i = 0; i < population.size(); i++) {
+                System.out.println("\nChromosome " + (i + 1) + ":");
+                System.out.println(population.get(i));
+            }
+        }
+
+        sc.close();
+    }
 
     public static ArrayList<ArrayList<Integer>> getDistanceBetweenAllPoints(int n) {
         ArrayList<ArrayList<Integer>> distanceMatrix = new ArrayList<>();
         Random rand = new Random();
+
         for (int i = 0; i < n; i++) {
             distanceMatrix.add(new ArrayList<>());
             for (int j = 0; j < n; j++) {
                 if (i == j) {
-                    distanceMatrix.get(i).add(0);
+                    distanceMatrix.get(i).add(Integer.valueOf(0));
                 } else if (j < i) {
                     distanceMatrix.get(i).add(distanceMatrix.get(j).get(i));
                 } else {
                     int distance = rand.nextInt(90) + 10;
-                    distanceMatrix.get(i).add(distance);
+                    distanceMatrix.get(i).add(Integer.valueOf(distance));
                 }
             }
         }
         return distanceMatrix;
+    }
+
+    public static void printDistanceMatrix(ArrayList<ArrayList<Integer>> matrix) {
+        System.out.print("     ");
+        for (int i = 0; i < matrix.size(); i++) {
+            System.out.printf("%4d", Integer.valueOf(i));
+        }
+        System.out.println();
+
+        for (int i = 0; i < matrix.size(); i++) {
+            System.out.printf("%4d:", Integer.valueOf(i));
+            for (int j = 0; j < matrix.get(i).size(); j++) {
+                System.out.printf("%4d", matrix.get(i).get(j));
+            }
+            System.out.println();
+        }
     }
 }
